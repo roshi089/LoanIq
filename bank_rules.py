2637afdfbbab8c21d0b364487e9147884f2327f5
roshi089 <ranjan.roshi8925@gmail.com>
2026-05-16 12:31:09 +0530

    Add files via upload

---
+++ bank_rules.py
@@ -0,0 +1,37 @@
+BANK_RULES = {
+
+    "SBI": {
+        "min_credit_score": 650,
+        "max_dti": 0.55,
+        "interest_rate": "8.50% - 10.15%",
+        "wait_period": "3-6 months"
+    },
+
+    "HDFC": {
+        "min_credit_score": 700,
+        "max_dti": 0.55,
+        "interest_rate": "8.70% - 9.95%",
+        "wait_period": "3-6 months"
+    },
+
+    "ICICI": {
+        "min_credit_score": 650,
+        "max_dti": 0.55,
+        "interest_rate": "8.75% - 10.05%",
+        "wait_period": "3-6 months"
+    },
+
+    "Axis": {
+        "min_credit_score": 650,
+        "max_dti": 0.50,
+        "interest_rate": "8.75% - 9.65%",
+        "wait_period": "3-6 months"
+    },
+
+    "Kotak": {
+        "min_credit_score": 720,
+        "max_dti": 0.50,
+        "interest_rate": "8.75% - 9.60%",
+        "wait_period": "6 months"
+    }
+}
